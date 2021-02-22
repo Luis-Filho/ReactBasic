a0d204e7d792dcea0e87b7ad08f8d4b8115ceda6
Luis <luis.filho@outlook.com.br>
2021-02-21 21:41:35 -0300

    Adicionado get e post no sistema de filme para teste.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,41 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
+const mysql = require("mysql");
+const { urlencoded } = require("body-parser");
 
-app.get('/', (req, res) =>{
-    res.send("hello World");
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'admin',
+    database: 'cruddatabase',
 });
 
-app.listen(3001, () =>{ 
+app.use(cors())
+app.use(express.json())
+//app.use(bodyParser, urlencoded({extended: true}))
+
+app.get('/api/get', (req, res) => {
+    const sqlSelect = "SELECT * FROM movie_reviews;";
+    db.query(sqlSelect, (err, result) => {
+        res.send(result);
+    });
+});
+
+app.post('/api/insert', (req, res) => {
+
+    const movieName = req.body.movieName;
+    const movieReview = req.body.movieReview;
+
+    const sqlInsert =
+        "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?);";
+    db.query(sqlInsert, [movieName, movieReview], (err, result) => {
+        res.send(result);
+    });
+
+});
+
+app.listen(3001, () => {
     console.log("running on port 3001");
 });
