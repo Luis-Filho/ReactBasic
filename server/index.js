59912412a7d36c3c246636813fd34c7c238228d5
Luis <luis.filho@outlook.com.br>
2021-02-22 20:51:09 -0300

    Final do crud update e delete

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const mysql = require("mysql");
-const { urlencoded } = require("body-parser");
+//const { urlencoded } = require("body-parser");
 
 const db = mysql.createPool({
     host: 'localhost',
@@ -35,6 +35,25 @@ app.post('/api/insert', (req, res) => {
     });
 
 });
+
+app.delete('/api/delete/:movieName', (req,res) =>{
+    const name = req.params.movieName
+    const sqlDelete =
+        "DELETE FROM movie_reviews WHERE movieName = ?";
+    db.query(sqlDelete,name, (err,result) => {
+        if (err) console.log(err)
+    })
+})
+
+app.put('/api/update', (req,res) =>{
+    const name = req.body.movieName
+    const review = req.body.movieReview
+    const sqlUpdate =
+        "UPDATE movie_reviews SET movieReview = ? WHERE movieName = ?";
+    db.query(sqlUpdate,[review, name], (err,result) => {
+        if (err) console.log(err)
+    })
+})
 
 app.listen(3001, () => {
     console.log("running on port 3001");
